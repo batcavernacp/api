1f3cca9c3225d9eba54fa0f2626676ca5aae4a9c
Rafael Santos <rafaelnsantos1@gmail.com>
2019-10-28 23:17:00 -0300

    add debug

--- src/google-actions.ts
+++ src/google-actions.ts
@@ -7,12 +7,25 @@ const controllers = controller({ services, repositories })
 
 const assistant = dialogflow()
 
-assistant.intent('portão batcaverna', conv => {
+function portao () {
+  if (process.env.DEBUG_DIALOGFLOW) {
+    console.log('portao')
+    return
+  }
   controllers.mqtt.switch('ON', 1)
   setTimeout(() => {
     controllers.mqtt.switch('OFF', 1)
   }, 75)
-  conv.ask('É pra já.')
+}
+
+assistant.intent('abrir portão', conv => {
+  portao()
+  conv.ask('Abrindo portão.')
+})
+
+assistant.intent('fechar portão', conv => {
+  portao()
+  conv.ask('Fechando portão.')
 })
 
 export { assistant }
