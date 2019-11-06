cf6aecb416e2851a7281567ede6e43df980195d8
Rafael Santos <rafaelnsantos1@gmail.com>
2019-11-05 21:42:36 -0300

    types

--- src/repositories/mongoose/index.ts
+++ src/repositories/mongoose/index.ts
@@ -1,7 +1,9 @@
 import { requireFolder } from '../../packages/folder-utils'
 import { DeviceModel } from './device-model'
 import { UserModel } from './user-model'
-// import {  } from 'mongodb'
+import { LogModel } from './log-model'
+import { PageInfo } from '../../generated/graphql'
+import { PageInput } from '../../graphql/schema'
 
 const inject = mongoose => requireFolder(__dirname, '-model', { mode: process.env.NODE_ENV === 'production' ? 'js' : 'ts', inject: mongoose })
 
@@ -10,9 +12,30 @@ export default inject
 export interface MongooseModels {
   Device: DeviceModel;
   User: UserModel;
+  Log: LogModel;
+}
+
+interface Connection {
+  edges: [Edge];
+  pageInfo: PageInfo;
+}
+
+interface Edge {
+  cursor: string;
+  node: {
+    id: string;
+  };
 }
 
 export interface Loader<T> {
   load: (id: string, info: any) => Promise<T>;
   loadMany: (ids: [string], info: any) => Promise<[T]>;
+}
+
+export interface RelayPagination<T> {
+  getPage: (input: GetPageInput) => Connection;
+}
+
+interface GetPageInput extends PageInput {
+  params: object;
 }
