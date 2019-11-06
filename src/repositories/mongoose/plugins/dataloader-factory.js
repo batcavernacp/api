a7e87b9c583da689c2bfb3428e9ac6eb97676461
Rafael Santos <rafaelnsantos1@gmail.com>
2019-11-05 21:37:32 -0300

    fix dataloader

--- src/repositories/mongoose/plugins/dataloader-factory.js
+++ src/repositories/mongoose/plugins/dataloader-factory.js
@@ -3,27 +3,16 @@ const ObjectId = require('mongodb').ObjectID
 const infoToProjection = require('./infoToProjection')
 
 const DataLoaderFactory = (function () {
-  function Loader (model, field) {
-    return new DataLoader(ids => model.find({ _id: { $in: ids.map(id => ObjectId(id)) } }, { [field]: 1 }))
-  }
-  function LoaderBeta (model, projection) {
+  function Loader (model, projection) {
     return new DataLoader(ids => model.find({ _id: { $in: ids.map(id => ObjectId(id)) } }, projection))
   }
   const instance = {}
   return {
-    getInstance: function (model, field, name) {
-      const key = name + field
-      if (!instance[key]) {
-        instance[key] = new Loader(model, field)
-        delete instance[key].constructor
-      }
-      return instance[key]
-    },
-    getInstanceBeta: function (model, info, name) {
+    getInstance: function (model, info, name) {
       const projection = infoToProjection(info)
       const key = name + JSON.stringify(projection)
       if (!instance[key]) {
-        instance[key] = new LoaderBeta(model, projection)
+        instance[key] = new Loader(model, projection)
         delete instance[key].constructor
       }
       return instance[key]
--- src/repositories/mongoose/plugins/mongoose-plugin-dataloader.js
+++ src/repositories/mongoose/plugins/mongoose-plugin-dataloader.js
@@ -2,10 +2,10 @@ const DataLoaderFactory = require('./dataloader-factory')
 
 module.exports = function (schema, { name }) {
   schema.statics.load = function (id, info) {
-    return DataLoaderFactory.getInstanceBeta(this, info, name).load(id)
+    return DataLoaderFactory.getInstance(this, info, name).load(id.toString())
   }
 
   schema.statics.loadMany = function (ids, info) {
-    return DataLoaderFactory.getInstanceBeta(this, info, name).loadMany(ids)
+    return DataLoaderFactory.getInstance(this, info, name).loadMany(ids.map(id => id.toString()))
   }
 }
