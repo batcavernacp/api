f7daf5e52328ad61df9600b1b0f4bcf6cd2c026b
Rafael Santos <rafaelnsantos1@gmail.com>
2019-11-05 21:44:09 -0300

    types

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -4,6 +4,13 @@ export interface Input<T> {
   input: T;
 }
 
+export interface PageInput {
+  after: string;
+  before: string;
+  first: number;
+  last: number;
+}
+
 export const schema = schemalizer(__dirname, {
   basePath: '',
   directives: '_directives',
