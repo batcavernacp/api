803fd5b73e8c290f81dc3b476bb3aa2334fdc457
Rafael Santos <rafaelnsantos1@gmail.com>
2019-11-05 21:45:31 -0300

    add Log type, add LogConnection to Device and resolvers

--- src/graphql/entities/device/device-entity-resolver.ts
+++ src/graphql/entities/device/device-entity-resolver.ts
@@ -1,4 +1,7 @@
 import { Context } from '../../../apollo'
+import { PageInput } from '../../schema'
+import { LogConnection } from '../../../generated/graphql'
+import { toGlobalId } from 'graphql-relay'
 
 exports.resolver = {
   Device: {
@@ -6,6 +9,26 @@ exports.resolver = {
       repositories.mongoose.models.Device.load(_doc.owner, info),
 
     usersInvited: ({ _doc }, _, { repositories }, info) =>
-      repositories.mongoose.models.User.loadMany(_doc.usersInvited, info)
+      repositories.mongoose.models.User.loadMany(_doc.usersInvited, info),
+
+    logs: ({ _doc }, page: PageInput, { repositories }: Context): LogConnection =>
+      repositories.mongoose.models.Log.getPage({
+        ...page,
+        params: {
+          device: _doc._id
+        }
+      })
+  },
+
+  LogEdge: {
+    node: ({ id }, _, { repositories }: Context, info) =>
+      repositories.mongoose.models.Log.load(id, info),
+
+    cursor: ({ id }) => toGlobalId('Log', id)
+  },
+
+  Log: {
+    user: ({ _doc }, _, { repositories }: Context, info) =>
+      repositories.mongoose.models.User.load(_doc.user, info)
   }
 }
